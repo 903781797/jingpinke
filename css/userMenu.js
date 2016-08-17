e710b30ae1fb93be799b5d3b87e4a36ad4f9e789
903781797 <zhaoqingchun163@163.com>
2016-08-17 13:29:30 +0800

    更新userMenu

---
+++ css/userMenu.js
@@ -0,0 +1,43 @@
+function userPanel() {
+	var model = "<div class='backoff '>" +
+		"<a href='#' class=' backoff-a glyphicon glyphicon-envelope '>" +
+		"<span class='badge' style='position: absolute; left: 45px;top: 10px; background-color: red;'>42</span>" +
+		"</a>" +
+		"</div>" +
+		"<div class='backoff' onmouseover='displaySubMenu()' onmouseout='hideSubMenu()'>" +
+		"<a href='#'  class='dropdown backoff-a' >" +
+		"<img src='img/touxiang.png' width='40px' height='40px' class=' img-circle' />" +
+		"</a>"
+
+	+"<div class='  mymenudown ' id='downmenu'>" +
+	"<div class='myarrow'></div>" +
+	"<div class='downusername'>欢迎您：小流氓0123</div>" +
+	"<div class='Setup'><a href='#' >个人设置</a><a href='javascript:loginOut();' style='float: right;' >退出</a></div>" +
+	"</div>" +
+	"</div>";
+	return model;
+}
+function userBtnModel(){
+	var model="<div id='loginbtn'>"
+                      +" 	<a href='javascript:loginIn();' class='mybtn'>登录</a>|"
+						+"<a href='#' class='mybtn '>注册</a>"
+                     +"   </div>";
+                     return model;
+}
+function displaySubMenu() {
+	var subMenu = document.getElementById('downmenu');
+	$("#downmenu").stop().slideToggle();
+	//subMenu.style.display = "block";
+}
+
+function hideSubMenu() {
+	var subMenu = document.getElementById('downmenu');
+	//subMenu.style.display = "none";
+	$("#downmenu").stop().slideToggle();
+}
+function loginIn(){
+	$("#loginmenu").html(userPanel());
+}
+function loginOut(){
+	$("#loginmenu").html(userBtnModel());	
+}
