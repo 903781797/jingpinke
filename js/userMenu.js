20d1b7538b55efb25d04bc2fe6edb31a7f0706e5
903781797 <zhaoqingchun163@163.com>
2016-09-13 09:05:05 +0800

    个人中心整改，大框完成

--- js/userMenu.js
+++ js/userMenu.js
@@ -41,3 +41,6 @@ function loginIn(){
 function loginOut(){
 	$("#loginmenu").html(userBtnModel());	
 }
+function getLoading(){
+	return "<div class='spinner'><div class='bounce1'></div><div class='bounce2'></div><div class='bounce3'></div></div>";
+}
